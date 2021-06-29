598bc00fdd6c5246a5ecbfeb1ab690ccaa2900ab
root <weiqiang.han>
2021-06-29 09:02:21 +0000

    [fix] cuda10.2 build

---
+++ bazel_build.sh
@@ -0,0 +1,119 @@
+#!/bin/bash
+# Build the components of tensorflow that require Bazel
+
+
+# Inputs:
+#       OUTPUT_DIRS - String of space-delimited directories to store outputs, in order of:
+#                       1)kernel test list
+#                       2)xla test list
+#                       3)tensorflow whl
+#                       4)other lib.so outputs
+#       TESTLIST - Determines whether the test lists are built (1 to build, 0 to skip)
+#       TRITON - Determines whether the TRITON-specific libary is built (1 to build, 0 to skip)
+#       NOCLEAN - Determines whether bazel clean is run and the tensorflow whl is
+#                       removed after the build and install (0 to clean, 1 to skip)
+#       BUILD_OPTS - File containing desired bazel flags for building tensorflow
+#       BAZEL_CACHE_FLAG - flag to add to BUILD_OPTS to enable bazel cache
+#       LIBCUDA_FOUND - Determines whether a libcuda stub was created and needs to be cleaned (0 to clean, 1 to skip)
+#       IN_CONTAINER - Flag for whether Tensorflow is being built within a container (1 for yes, 0 for bare-metal)
+#       TF_API - TensorFlow API version: 1 => v1.x, 2 => 2.x
+#
+
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+
+
+read -ra OUTPUT_LIST <<<"$OUTPUT_DIRS"
+KERNEL_OUT=${OUTPUT_LIST[0]}
+XLA_OUT=${OUTPUT_LIST[1]}
+WHL_OUT=${OUTPUT_LIST[2]}
+LIBS_OUT=${OUTPUT_LIST[3]}
+
+
+for d in ${OUTPUT_LIST[@]}
+do
+  mkdir -p ${d}
+done
+
+KERNEL_TEST_RETURN=0
+BAZEL_BUILD_RETURN=0
+if [[ "$TF_API" == "2" ]]; then
+  BAZEL_OPTS="--config=v2 $(cat $BUILD_OPTS) $BAZEL_CACHE_FLAG"
+else
+  BAZEL_OPTS="$(cat $BUILD_OPTS) $BAZEL_CACHE_FLAG"
+fi
+echo "BAZEL_OPTS: $BAZEL_OPTS"
+
+
+TRITON_TARGET=
+if [[ $TRITON -eq 1 ]]; then
+  TRITON_TARGET="//tensorflow:libtensorflow_triton.so"
+fi
+if [[ $IN_CONTAINER -eq 1 ]]; then
+  bazel build $BAZEL_OPTS \
+      tensorflow/tools/pip_package:build_pip_package \
+      //tensorflow:libtensorflow_cc.so $TRITON_TARGET
+  BAZEL_BUILD_RETURN=$?
+  cp bazel-bin/tensorflow/libtensorflow_cc.so.? ${LIBS_OUT}
+  if [[ $TRITON -eq 1 ]]; then
+    cp bazel-bin/tensorflow/libtensorflow_triton.so.? ${LIBS_OUT}
+  fi
+else
+  bazel build $BAZEL_OPTS \
+      tensorflow/tools/pip_package:build_pip_package $TRITON_TARGET
+  if [[ $TRITON -eq 1 ]]; then
+    cp bazel-bin/tensorflow/libtensorflow_triton.so.? ${LIBS_OUT}
+  fi
+  BAZEL_BUILD_RETURN=$?
+fi
+
+if [ ${BAZEL_BUILD_RETURN} -gt 0 ]
+then
+  exit ${BAZEL_BUILD_RETURN}
+fi
+# Build the test lists for L1 kernel and xla tests
+if [[ $TESTLIST -eq 1 ]]; then
+  rm -f "${KERNEL_OUT}/tests.list" \
+        "${XLA_OUT}/tests.list"
+
+  bazel test --verbose_failures --local_test_jobs=1 \
+             --run_under="$THIS_DIR/test_grabber.sh $KERNEL_OUT" \
+             --build_tests_only --test_tag_filters=-no_gpu,-benchmark-test \
+             --cache_test_results=no $BAZEL_OPTS -- \
+             //tensorflow/python/kernel_tests/... \
+             `# The following tests are skipped becaues they depend on additional binaries.` \
+             -//tensorflow/python/kernel_tests:ackermann_test \
+             -//tensorflow/python/kernel_tests:duplicate_op_test \
+             -//tensorflow/python/kernel_tests:invalid_op_test \
+             -//tensorflow/python/kernel_tests/proto:encode_proto_op_test \
+             -//tensorflow/python/kernel_tests/proto:decode_proto_op_test \
+             -//tensorflow/python/kernel_tests/proto:descriptor_source_test \
+             `# Enable the following when nvbug 3098132 is fixed.` \
+             -//tensorflow/python/kernel_tests:conv_ops_3d_test \
+             -//tensorflow/python/kernel_tests:conv_ops_3d_test_gpu
+      KERNEL_TEST_RETURN=$?
+fi
+
+bazel-bin/tensorflow/tools/pip_package/build_pip_package $WHL_OUT --gpu --project_name tensorflow
+PIP_PACKAGE_RETURN=$?
+if [ ${PIP_PACKAGE_RETURN} -gt 0 ]; then
+  echo "Assembly of TF pip package failed."
+  exit ${PIP_PACKAGE_RETURN}
+fi
+
+pip install --no-cache-dir --upgrade $WHL_OUT/tensorflow-*.whl
+PIP_INSTALL_RETURN=$?
+if [ ${PIP_INSTALL_RETURN} -gt 0 ]; then
+  echo "Installation of TF pip package failed."
+  exit ${PIP_INSTALL_RETURN}
+fi
+
+if [[ $NOCLEAN -eq 0 ]]; then
+  rm -f $WHL_OUT/tensorflow-*.whl
+  bazel clean --expunge
+  rm .tf_configure.bazelrc
+  rm -rf ${HOME}/.cache/bazel /tmp/*
+  if [[ "$LIBCUDA_FOUND" -eq 0 ]]; then
+    rm /usr/local/cuda/lib64/stubs/libcuda.so.1
+  fi
+fi
+
---
+++ nvbuild.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+#
+# Configure, build, and install Tensorflow
+#
+
+# Exit at error
+set -e
+
+Usage() {
+  echo "Configure, build, and install Tensorflow."
+  echo ""
+  echo "  Usage: $0 [OPTIONS]"
+  echo ""
+  echo "    OPTIONS                        DESCRIPTION"
+  echo "    --configonly                   Run configure step only"
+  echo "    --noconfig                     Skip configure step"
+  echo "    --noclean                      Retain intermediate build files"
+  echo "    --testlist                     Build list of python kernel_tests"
+  echo "    --v1                           Build TensorFlow v1 API"
+  echo "    --v2                           Build TensorFlow v2 API"
+  echo "    --bazel-cache                  Use Bazel build cache"
+  echo "    --bazel-cache-download-only    Use Bazel build cache in download mode only. No cache upload"
+}
+
+PYVER=3.8
+CONFIGONLY=0
+NOCONFIG=0
+NOCLEAN=0
+TESTLIST=0
+TRITON=0
+TF_API=1
+BAZEL_CACHE=0
+BAZEL_CACHE_NOUPLOAD=0
+
+while [[ $# -gt 0 ]]; do
+  case $1 in
+    "--help"|"-h")   Usage; exit 1 ;;
+    "--configonly")  CONFIGONLY=1 ;;
+    "--noconfig")    NOCONFIG=1 ;;
+    "--noclean")     NOCLEAN=1 ;;
+    "--testlist")    TESTLIST=1 ;;
+    "--triton")       TRITON=1 ;;
+    "--v1")          TF_API=1 ;;
+    "--v2")          TF_API=2 ;;
+    "--bazel-cache") BAZEL_CACHE=1 ;;
+    "--bazel-cache-download-only") BAZEL_CACHE_NOUPLOAD=1 ;;
+    *)
+      echo UNKNOWN OPTION $1
+      echo Run $0 -h for help
+      exit 1
+  esac
+  shift 1
+done
+
+export TF_NEED_CUDA=1
+export TF_NEED_TENSORRT=1
+export TF_CUDA_PATHS=/usr,/usr/local/cuda
+export TF_CUDA_VERSION=$(echo "${CUDA_VERSION}" | cut -d . -f 1-2)
+export TF_CUBLAS_VERSION=$(echo "${CUBLAS_VERSION}" | cut -d . -f 1)
+export TF_CUDNN_VERSION=$(echo "${CUDNN_VERSION}" | cut -d . -f 1)
+export TF_NCCL_VERSION=$(echo "${NCCL_VERSION}" | cut -d . -f 1)
+export TF_TENSORRT_VERSION=$(echo "${TRT_VERSION}" | cut -d . -f 1)
+if [[ "$TF_CUDA_VERSION" < "11.0" ]];then 
+    export TF_CUDA_COMPUTE_CAPABILITIES="5.2,6.0,6.1,7.0,7.5"
+else
+    export TF_CUDA_COMPUTE_CAPABILITIES="5.2,6.0,6.1,7.0,7.5,8.0,8.6"
+fi
+export TF_ENABLE_XLA=1
+export TF_NEED_HDFS=0
+export TF_NEED_MKL=1
+export TF_NEED_NUMA=1
+#export CC_OPT_FLAGS="-march=sandybridge -mtune=broadwell -Wno-sign-compare"
+export CC_OPT_FLAGS="-march=skylake -mtune=skylake -Wno-sign-compare"
+
+THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
+export PYTHON_BIN_PATH=$(which python)
+LIBCUDA_FOUND=$(ldconfig -p | awk '{print $1}' | grep libcuda.so | wc -l)
+if [[ $NOCONFIG -eq 0 ]]; then
+  if [[ "$LIBCUDA_FOUND" -eq 0 ]]; then
+      export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64/stubs
+      ln -fs /usr/local/cuda/lib64/stubs/libcuda.so /usr/local/cuda/lib64/stubs/libcuda.so.1
+  fi
+  yes "" | ./configure
+fi
+
+if [[ $CONFIGONLY -eq 1 ]]; then
+  exit 0
+fi
+
+unset BAZEL_CACHE_FLAG
+if [[ $BAZEL_CACHE -eq 1 || $BAZEL_CACHE_NOUPLOAD -eq 1 ]]; then
+  export BAZEL_CACHE_FLAG="$(${THIS_DIR}/nvbazelcache)"
+fi
+if [[ $BAZEL_CACHE_NOUPLOAD -eq 1 && ! -z "$BAZEL_CACHE_FLAG" ]]; then
+  export BAZEL_CACHE_FLAG="$BAZEL_CACHE_FLAG --remote_upload_local_results=false"
+fi
+echo "Bazel Cache Flag: $BAZEL_CACHE_FLAG"
+
+export OUTPUT_DIRS="tensorflow/python/kernel_tests tensorflow/compiler/tests /tmp/pip /usr/local/lib/tensorflow"
+export BUILD_OPTS="${THIS_DIR}/nvbuildopts"
+export IN_CONTAINER="1"
+export TESTLIST
+export TRITON
+export NOCLEAN
+export LIBCUDA_FOUND
+export TF_API
+bash ${THIS_DIR}/bazel_build.sh
+
